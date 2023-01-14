3fedc94d85c353698ae7939425c057d30929cf0f
ethmth <ethan@smaze.net>
2023-01-14 17:34:06 -0500

    Added redundancy to check_online test

--- ipcheck.sh
+++ ipcheck.sh
@@ -8,8 +8,23 @@ ABSOLUTE_PATH=$(pwd)
 # CHECK IF THERE IS INTERNET CONNECTION
 function check_online
 {
-    netcat -z -w 5 1.1.1.1 53 && echo 1 || echo 0
+    ON_NOW=$(netcat -z -w 5 1.1.1.1 53 && echo 1 || echo 0)
+	if [ $ON_NOW -eq 1 ]; then
+		echo $ON_NOW
+	else
+		echo "PING TEST"
+		ping -c 1 -W 3 archlinux.org && echo 1 || echo 0
+	fi
 }
+
+function ifttt_request
+{
+	echo "IP is $IP_ADDRESS"
+    #curl -o /dev/null -X POST -H "Content-Type: application/json" -d "{\"ip-info\": \"${IP_ADDRESS}\"}" https://maker.ifttt.com/trigger/${EVENT_NAME}/json/with/key/${IFTTT_KEY}
+}
+
+#check_online
+#exit 0
 
 IS_ONLINE=$(check_online)
 
@@ -17,7 +32,7 @@ MAX_CHECKS=20
 CHECKS=0
 
 while [ $IS_ONLINE -eq 0 ]; do
-
+	echo "SLEEPING 10"
     sleep 10;
     IS_ONLINE=$(check_online)
 
@@ -31,6 +46,8 @@ if [ $IS_ONLINE -eq 0 ]; then
     exit 1
 fi
 
+echo "INTERNET ON"
+
 # GET THE LOCAL IP
 
 source "${ABSOLUTE_PATH}/.env"
@@ -41,10 +58,13 @@ echo ${IP_ADDRESS} > ${ABSOLUTE_PATH}/ip_temp.txt
 
 IP_ADDRESS=$(cat ${ABSOLUTE_PATH}/ip_temp.txt)
 
+echo "IN BODY IP: $IP_ADDRESS"
+
 # SEND THE LOCAL IP TO IFTTT ON startup ARG
 
 if [[ "$1" == "startup" ]]; then
-    curl -o /dev/null -X POST -H "Content-Type: application/json" -d "{\"ip-info\": \"${IP_ADDRESS}\"}" https://maker.ifttt.com/trigger/${EVENT_NAME}/json/with/key/${IFTTT_KEY}
+    #curl -o /dev/null -X POST -H "Content-Type: application/json" -d "{\"ip-info\": \"${IP_ADDRESS}\"}" https://maker.ifttt.com/trigger/${EVENT_NAME}/json/with/key/${IFTTT_KEY}
+	ifttt_request
 
     echo ${IP_ADDRESS} > ${ABSOLUTE_PATH}/ip_state.txt
 
@@ -58,7 +78,8 @@ OLD_IP_ADDRESS=$(cat ${ABSOLUTE_PATH}/ip_state.txt)
 if [[ "$OLD_IP_ADDRESS" == "$IP_ADDRESS" ]]; then
     echo "ip hasn't changed"
 else
-    curl -o /dev/null -X POST -H "Content-Type: application/json" -d "{\"ip-info\": \"${IP_ADDRESS}\"}" https://maker.ifttt.com/trigger/${EVENT_NAME}/json/with/key/${IFTTT_KEY}
+    #curl -o /dev/null -X POST -H "Content-Type: application/json" -d "{\"ip-info\": \"${IP_ADDRESS}\"}" https://maker.ifttt.com/trigger/${EVENT_NAME}/json/with/key/${IFTTT_KEY}
+	ifttt_request
 fi
 
 echo ${IP_ADDRESS} > ${ABSOLUTE_PATH}/ip_state.txt
